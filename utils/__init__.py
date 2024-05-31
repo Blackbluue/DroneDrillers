ef8585fec3f315e687a69599650ce091e98028a9
Raymone Miller <miller_raymone@hotmail.com>
2024-05-31 00:04:39 -0400

    Define base ally unit

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+from .context import Context
+from .coordinate import Coordinate
+from .directions import Directions
+from .icon import Icon
+
+# from .map import Map
+# from .tile import Tile
+
+# static constants that others may need
+DEFAULT_COORDINATE = Coordinate(0, 0)
+# DEFAULT_TILE = Tile(DEFAULT_COORDINATE, Icon.UNKNOWN)
