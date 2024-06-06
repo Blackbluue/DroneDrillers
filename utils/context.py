98f5e3eff93eb9ccfda4fa2fe5dc8d2615768224
Raymone Miller <raymone.c.miller.mil@army.mil>
2024-06-06 13:11:14 -0400

    Update default values for Context

--- utils/context.py
+++ utils/context.py
@@ -10,20 +10,20 @@ class Context(NamedTuple):
     """A context object, used to describe the surrounding's of a drone.
 
     Attributes:
-        x (int, optional): The X coordinate of the drone. Defaults to 0.
-        y (int, optional): The Y coordinate of the drone. Defaults to 0.
+        x (int, optional): The X coordinate of the drone. Defaults to -1.
+        y (int, optional): The Y coordinate of the drone. Defaults to -1.
         north (Icon, optional): The tile to the north of the drone.
-            Defaults to Icon.EMPTY.
+            Defaults to Icon.UNKNOWN.
         south (Icon, optional): The tile to the south of the drone.
-            Defaults to Icon.EMPTY.
+            Defaults to Icon.UNKNOWN.
         east (Icon, optional): The tile to the east of the drone.
-            Defaults to Icon.EMPTY.
+            Defaults to Icon.UNKNOWN.
         west (Icon, optional): The tile to the west of the drone.
-            Defaults to Icon.EMPTY.
+            Defaults to Icon.UNKNOWN.
     """
 
-    coord: Coordinate = Coordinate(0, 0)
-    north: Icon | None = Icon.EMPTY
-    south: Icon | None = Icon.EMPTY
-    east: Icon | None = Icon.EMPTY
-    west: Icon | None = Icon.EMPTY
+    coord: Coordinate = Coordinate(-1, -1)
+    north: Icon | None = Icon.UNKNOWN
+    south: Icon | None = Icon.UNKNOWN
+    east: Icon | None = Icon.UNKNOWN
+    west: Icon | None = Icon.UNKNOWN
