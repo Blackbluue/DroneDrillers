a2293cc365c47a8397fc1d6b4fe280c70c716860
Raymone Miller <miller_raymone@hotmail.com>
2024-06-05 22:16:32 -0400

    Remove overloaded methods

--- utils/coordinate.py
+++ utils/coordinate.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-from functools import singledispatchmethod
 from typing import NamedTuple
 
 from .directions import Directions
@@ -14,7 +13,6 @@ class Coordinate(NamedTuple):
     x: int
     y: int
 
-    @singledispatchmethod
     def difference(self, other_coord: Coordinate) -> tuple[int, int]:
         """Get the difference in distance between 2 coordinates.
 
@@ -31,26 +29,6 @@ class Coordinate(NamedTuple):
         """
         return (other_coord.x - self.x, other_coord.y - self.y)
 
-    @difference.register
-    def _(self, x: int, y: int) -> tuple[int, int]:
-        """Get the difference in distance between 2 coordinates.
-
-        This method allows for the other point to be given as 2 ints for x, y.
-        In relation to this coordinate, a positive/negative return value will
-        indicate direction; a positive  x means the other coordinate is to the
-        right of this one, while a positive y means the other coordinate is
-        below this one, and reversed for negative values.
-
-        Args:
-            x (int): The x value of the other coordinate.
-            y (int): The y value of the other coordinate.
-
-        Returns:
-            tuple[int, int]: The distance difference, as (x, y) tuple.
-        """
-        return (x - self.x, y - self.y)
-
-    @singledispatchmethod
     def direction(self, other_coord: Coordinate) -> str:
         """Get the direction of the other coordinate in relation to this one.
 
@@ -64,25 +42,7 @@ class Coordinate(NamedTuple):
         Returns:
             str: The direction of the other coordinate.
         """
-        return self.direction(*other_coord)
-
-    @direction.register
-    def _(self, x: int, y: int) -> str:
-        """Get the direction of the other coordinate in relation to this one.
-
-        This method allows for the other point to be given as 2 ints for x, y.
-        Note that this only works if this coordinate and the other are on the
-        same axis as each other. Otherwise an empty string will be returned.
-        If the 2 coordinates are the same, 'center' will be returned.
-
-        Args:
-            x (int): The x value of the other coordinate.
-            y (int): The y value of the other coordinate.
-
-        Returns:
-            str: The direction of the other coordinate.
-        """
-        x_offset, y_offset = self.difference(x, y)
+        x_offset, y_offset = self.difference(other_coord)
         if x_offset and y_offset:
             return ""  # can't determine direction
         if x_offset > 0:
