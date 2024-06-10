91e44954fa37cf2553c22aea7f6f2537e4edfb1f
Raymone Miller <miller_raymone@hotmail.com>
2024-06-09 23:36:27 -0400

    Add fog of war to map

--- gui/graphic_tile.py
+++ gui/graphic_tile.py
@@ -5,6 +5,9 @@ from __future__ import annotations
 from tkinter import Label
 from typing import TYPE_CHECKING
 
+from utils import Icon
+from utils.icon_var import IconVar
+
 if TYPE_CHECKING:
     from tkinter import Frame
 
@@ -21,13 +24,22 @@ class GraphicTile(Label):
             master: The parent widget.
             tile (Tile): The Tile.
         """
-        super().__init__(master, textvariable=tile.icon_var)
+        super().__init__(master)
         self._map_frame = master
         self._tile = tile
-
+        self._visible_icon = IconVar(master=self)
+        self["textvariable"] = self._visible_icon
+        tile.icon_var.trace_add("write", self._update_tile)
+        tile.discovered.trace_add("write", self._update_tile)
+        self["width"] = 1
+        self["relief"] = "raised"
         self.grid(row=tile.coordinate.y, column=tile.coordinate.x)
 
-    @property
-    def tile(self):
-        """The tile."""
-        return self._tile
+    def _update_tile(self, *_) -> None:
+        """Change the tile icon."""
+        if self._tile.discovered.get():
+            self._visible_icon.set(self._tile.icon)
+            self["bg"] = "white"
+        else:
+            self._visible_icon.set(Icon.UNKNOWN)
+            self["bg"] = "grey"
