0b2b15421981b8c2eec03ba485e46a5b5a981449
Raymone Miller <miller_raymone@hotmail.com>
2024-06-29 01:14:43 -0400

    Remove legend table

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -34,24 +34,19 @@ class Dashboard(tk.Frame):
             parent (tk.Tk): Takes in a tk top level window
         """
         super().__init__(parent)
-        self.photo = tk.PhotoImage(file="icon.png")
-        self.configure(bg="#2C292C")
 
-        # Configure the style of Heading in Treeview widget
-        self._prep_dashboard_trees()
-        self.legend_insertion()
         self._player_health = LabeledCounter(
-            self,
-            "Player Health:",
-            counter=player_health,
+            self, "Player Health:", counter=player_health
         )
         self._ticks = LabeledCounter(self, "Ticks:", counter=ticks)
         self._refined = LabeledCounter(
             self, "Refined Minerals:", counter=minerals
         )
-        self._player_health.grid(row=0, column=0)
-        self._ticks.grid(row=0, column=1)
-        self._refined.grid(row=0, column=2)
+
+        self._prep_dashboard_trees()
+        self._player_health.pack()
+        self._ticks.pack()
+        self._refined.pack()
 
     def _make_tree(self, labels: Mapping[str, int]) -> ttk.Treeview:
         """Build trees for the dashboard to use.
@@ -63,8 +58,6 @@ class Dashboard(tk.Frame):
         """
         style = ttk.Style()
         style.theme_use("clam")
-
-        # Configure the style of Heading in Treeview widget
         style.configure("Treeview.Heading", background="#ad73ac")
 
         # Using treeview widget
@@ -82,32 +75,16 @@ class Dashboard(tk.Frame):
             tree_view.heading(string_column, text=column)
         return tree_view
 
-    def legend_insertion(self) -> None:
-        """Prepare the legend in the dashboard."""
-        for key, unicode in Icon.unicode_mappings().items():
-            self.legend_tree.insert(
-                "",
-                "end",
-                text="Listbox",
-                values=(unicode, key),
-            )
-
     def _prep_dashboard_trees(self) -> None:
         """Prepare the three tree views in the dashboard."""
-        # https://www.geeksforgeeks.org/python-tkinter-treeview-scrollbar/
-        legend_labels = {"Map Symbol": 180, "Meaning": 180}
-
         drone_labels = {
             "Drone ID": 180,
             "Drone Type": 120,
             "Health": 90,
             "Capacity": 90,
         }
-        padding = (20, 20)
-        self.legend_tree = self._make_tree(legend_labels)
-        self.legend_tree.grid(row=1, column=0, padx=padding, pady=padding)
         self.drone_tree = self._make_tree(drone_labels)
-        self.drone_tree.grid(row=1, column=1, padx=padding, pady=padding)
+        self.drone_tree.pack(side="left")
 
     def add_atron_to_tree(self, new_drone: Atron) -> None:
         """Add a drone to the drone tree in the gui.
